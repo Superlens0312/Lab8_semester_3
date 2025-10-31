4c12c97acd2ba5edb9aaf8ab2c4412e3693e71c7
Lenny Manset <Lenny.manset@gmail.com>
2025-10-31 13:33:36 -0400

    initial commit(Base implementation)

---
+++ src/Scenebuilder.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author 2488638
+ */
+public class Scenebuilder extends Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException  {
+ 
+           Parent root = FXMLLoader.load(getClass().getResource("FXML.fxml"));
+           
+        
+        stage.setScene(new Scene(root));
+        stage.setTitle("FXML example");
+        stage.show();
+    }
+    
+}
